21c66962c2a18c1539331208c1232a3765cadd02
Sara <sara2718@gmail.com>
2020-05-23 12:26:15 +0200

    Spider Generated

---
+++ quotes/spiders/QuoteSpider.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class QuotespiderSpider(scrapy.Spider):
+    name = 'QuoteSpider'
+    allowed_domains = ['quotes.toscrape.com']
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        pass
